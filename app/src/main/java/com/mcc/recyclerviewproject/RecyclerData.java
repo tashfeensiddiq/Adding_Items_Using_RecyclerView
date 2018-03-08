c7df7e2d9266bba3e7fd47c2eda71ba839f03b31
Tashfeen Siddiq <tashfeensiddiq.mcc@gmail.com>
2018-03-08 12:38:09 +0600

    Initial commit

---
+++ app/src/main/java/com/mcc/recyclerviewproject/RecyclerData.java
@@ -0,0 +1,31 @@
+package com.mcc.recyclerviewproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+
+/**
+ * Created by Admin on 3/8/2018.
+ */
+
+public class RecyclerData {
+
+    String title;
+    String description;
+    RecyclerView data;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setCrossImage(ImageView crossImage){
+        setCrossImage(crossImage);
+    }
+
+}
